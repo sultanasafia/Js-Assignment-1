51d89a210bf52ba1994b8a593d3487964887c040
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-10-30 18:16:49 +0500

    Js-Ass-1

---
+++ app.js
@@ -0,0 +1,16 @@
+document.write("Assignment-1")
+//Q1===================
+// alert("Hello!")
+//Q2================
+// alert("Error! please enter a valid password")
+//Q3================
+// alert('Welcome to js land...\n Happy coding!')
+//Q4================
+// alert('Welcome to js land')
+// alert('Happy Coding!')
+//Q5================
+// alert("Hello! I can run js through my web browser's console")
+//Q6================
+// alert("Hello! Everyone")
+//Assignment-1 completed=================================
+
